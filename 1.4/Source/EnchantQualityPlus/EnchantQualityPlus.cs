1fe0659181808ea0fa80310654f6a2b2eaadd12b
StatistNo1 <80486109+StatistNo1@users.noreply.github.com>
2023-09-15 01:53:50 +0200

    Update to match the Vanilla Psycast Expanded update

--- 1.4/Source/EnchantQualityPlus/EnchantQualityPlus.cs
+++ 1.4/Source/EnchantQualityPlus/EnchantQualityPlus.cs
@@ -15,7 +15,7 @@ namespace EnchantQualityPlus.Technomancer {
     }
     public class GameComponent : Verse.GameComponent {
         Game game = null;
-        public GameComponent(Verse.Game game) {this.game = game;}
+        public GameComponent(Verse.Game game) { this.game = game; }
         public override void LoadedGame() {
             base.LoadedGame();
             foreach (Map m in game.Maps) {
@@ -23,19 +23,36 @@ namespace EnchantQualityPlus.Technomancer {
                     if (p.RaceProps.Humanlike == true && p.HasPsylink) {
                         System.Collections.Generic.List<Ability> list = p.GetComp<CompAbilities>().LearnedAbilities;
                         Ability a = p.GetComp<CompAbilities>().LearnedAbilities.Find(x => x is VanillaPsycastsExpanded.Technomancer.Ability_IncreaseQuality);
-                        list.Remove(a);
-                        p.GetComp<CompAbilities>().GiveAbility(a.def);
+                        if (a != null) {
+                            list.Remove(a);
+                            p.GetComp<CompAbilities>().GiveAbility(a.def);
+                        }
                     }
                 }
             }
         }
+        public override void FinalizeInit() {
+            base.FinalizeInit();
+            if (!Settings.ImportantMessage20230915) {
+                Verse.Find.LetterStack.ReceiveLetter("EnchantQualityPlus info",
+                    "Vanilla Psycast Expanded made an update to the Enchant Quality psycast, which means I had to do one as well.\n" +
+                    "The updated psycasts maximum quality is now dependant on your casters psychic sensitivity.\n" +
+                    "There is a setting that lets you decide how to handle the increased maximum quality of this mod.\n" +
+                    "Don't like the scaling mechanic? There is a setting to go back to the old Enchant Quality psycast as well!\n",
+                    LetterDefOf.NeutralEvent);
+                Settings.ImportantMessage20230915 = true;
+                Verse.LoadedModManager.GetMod<EvolvedOrgansReduxSettings>().WriteSettings();
+
+            }
+        }
     }
-        public class Ability_IncreaseQuality : Ability {
+    public class Ability_IncreaseQuality : Ability {
         public override void Cast(params GlobalTargetInfo[] targets) {
             base.Cast(targets);
+
             foreach (GlobalTargetInfo target in targets) {
                 CompQuality comp = target.Thing.GetInnerIfMinified().TryGetComp<CompQuality>();
-                if (comp is not { Quality: < QualityCategory.Legendary }) return;
+                if (comp == null || comp.Quality >= MaxQuality) return;
                 comp.SetQuality(comp.Quality + 1, ArtGenerationContext.Colony);
                 for (int i = 0; i < 16; i++) FleckMaker.ThrowMicroSparks(target.Thing.TrueCenter(), this.pawn.Map);
             }
@@ -43,19 +60,49 @@ namespace EnchantQualityPlus.Technomancer {
 
         public override bool ValidateTarget(LocalTargetInfo target, bool showMessages = true) {
             if (!base.ValidateTarget(target, showMessages)) return false;
-
             CompQuality comp;
             if ((comp = target.Thing.GetInnerIfMinified().TryGetComp<CompQuality>()) == null) {
                 if (showMessages) Messages.Message("VPE.MustHaveQuality".Translate(), MessageTypeDefOf.RejectInput, false);
                 return false;
             }
-
-            if (comp.Quality >= QualityCategory.Legendary) {
-                if (showMessages) Messages.Message("VPE.QualityTooHigh".Translate(), MessageTypeDefOf.RejectInput, false);
+            if (comp.Quality >= MaxQuality) {
+                if (showMessages) Messages.Message("VPE.QualityTooHigh".Translate(MaxQuality.GetLabel()), MessageTypeDefOf.RejectInput, false);
                 return false;
             }
 
             return true;
+        }
+
+        private QualityCategory MaxQuality {
+            get {
+                if (Settings.ChoiceOfMechanic == "LegacyExcellent")
+                    return QualityCategory.Excellent;
+                else if (Settings.ChoiceOfMechanic == "LegacyMasterwork")
+                    return QualityCategory.Masterwork;
+                else if (Settings.ChoiceOfMechanic == "LegacyLegendary")
+                    return QualityCategory.Legendary;
+                else return (QualityCategory)(int)GetPowerForPawn();
+            }
+        }
+
+        public override string GetPowerForPawnDescription() => "VPE.MaxQuality".Translate(MaxQuality.GetLabel()).Colorize(UnityEngine.Color.cyan);
+        public override float GetPowerForPawn() {
+            if (Settings.ChoiceOfMechanic == "ScalingSquished")
+                return pawn.GetStatValue(StatDefOf.PsychicSensitivity) switch {
+                    <= 1.2f => (int)QualityCategory.Good,
+                    <= 1.5f => (int)QualityCategory.Excellent,
+                    <= 2.0f => (int)QualityCategory.Masterwork,
+                    > 2.5f => (int)QualityCategory.Legendary,
+                    _ => (int)QualityCategory.Normal
+                };
+            else
+                return pawn.GetStatValue(StatDefOf.PsychicSensitivity) switch {
+                    <= 1.2f => (int)QualityCategory.Good,
+                    <= 2f => (int)QualityCategory.Excellent,
+                    <= 2.5f => (int)QualityCategory.Masterwork,
+                    > 3.5f => (int)QualityCategory.Legendary,
+                    _ => (int)QualityCategory.Normal
+                };
         }
     }
 }
---
+++ 1.4/Source/EnchantQualityPlus/Settings.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnchantQualityPlus {
+    public class Settings : Verse.ModSettings {
+        public static bool ImportantMessage20230915 = false;
+        public static string ChoiceOfMechanic = "ScalingSquished";
+        public List<string> ChoicesOfMechanic = new() { "ScalingSquished", "ScalingAdded", "LegacyLegendary", "LegacyMasterwork", "LegacyExcellent" };
+        public override void ExposeData() {
+            Verse.Scribe_Values.Look(ref ImportantMessage20230915, "ImportantMessage20230915");
+            Verse.Scribe_Values.Look(ref ChoiceOfMechanic, "ChoiceOfMode");
+            base.ExposeData();
+        }
+    }
+
+    public class EvolvedOrgansReduxSettings : Verse.Mod {
+        readonly Settings settings;
+        public EvolvedOrgansReduxSettings(Verse.ModContentPack content) : base(content) {
+            this.settings = GetSettings<Settings>();
+        }
+        public override void DoSettingsWindowContents(UnityEngine.Rect inRect) {
+            Verse.Listing_Standard listingStandard = new Verse.Listing_Standard();
+            listingStandard.Begin(inRect);
+            listingStandard.Label("Which mode do you want to use?");
+            listingStandard.Label("ScalingSquished squishes the scaling down and unlocks the Legendary enchant with 250% psychic sensitivity.");
+            listingStandard.Label("ScalingAdded keeps the base scaling and just adds the Legendary enchant unlock at 350% psychich sensitivity.");
+            listingStandard.Label("LegacyLegendary has the same mechanic as it used to have. Items can be enchanted until Legendary.");
+            listingStandard.Label("LegacyLegendary has the same mechanic as it used to have. Items can be enchanted until Masterwork.");
+            listingStandard.Label("LegacyLegendary has the same mechanic as it used to have. Items can be enchanted until Excellent.");
+            listingStandard.Gap();
+            for (int i = 0; i < settings.ChoicesOfMechanic.Count; i++) {
+                if (listingStandard.RadioButton(settings.ChoicesOfMechanic[i], Settings.ChoiceOfMechanic == settings.ChoicesOfMechanic[i], tabIn: 30f)) {
+                    Settings.ChoiceOfMechanic = settings.ChoicesOfMechanic[i];
+                }
+            }
+            listingStandard.End();
+        }
+        public override string SettingsCategory() {
+            return "EnchantQualityPlus";
+        }
+    }
+}
